4f19be182ea69d6add0e5811701ce80e7faa3700
Kenneth <kenneth.skertchly@silvercar.com>
2019-02-06 23:37:15 -0600

    Fixed security group templates
    Each security group rule needs its own unique resource name. When terraform imports a security group, it also imports the attached rules, gives them the name of the security group, and appends an incrementing number. In order to emulate this, I added a global counter function that can be used across multiple jinja template calls.

--- src/terrafort/main.py
+++ src/terrafort/main.py
@@ -5,7 +5,10 @@ import json
 import click
 import boto3
 import jinja2
+import subprocess
 from botocore.exceptions import ClientError
+
+counter = 0
 
 
 @click.group()
@@ -15,41 +18,63 @@ def cli():
 
 @click.command('sg')
 @click.argument('id')
-def get_security_group(id):
+def sg(id):
     ec2 = boto3.client('ec2')
     try:
         response = ec2.describe_security_groups(GroupIds=[id])
-        group = response['SecurityGroups'][0]
-        print(json.dumps(group, indent=4, sort_keys=True))
-        # output(group, 'aws_security_group.tf')
-        for permission in group['IpPermissions']:
-            permission['id'] = group['GroupId']
-            permission['type'] = "ingress"
-            permission['name'] = group['GroupName']
-            output(permission, 'aws_security_group_rule.tf')
-
-        for permission in group['IpPermissionsEgress']:
-            permission['id'] = group['GroupId']
-            permission['type'] = "egress"
-            permission['name'] = group['GroupName']
-            output(permission, 'aws_security_group_rule.tf')
-
-
-
-        # print(response)
     except ClientError as e:
         print(e)
+        return
+
+    group = response['SecurityGroups'][0]
+    # print(json.dumps(group, indent=4, sort_keys=True))
+
+    output = render(group, 'aws_security_group.tf')
+
+    reset_count()  # Need this to add a numeric suffix to each rule name
+    for num, permission in enumerate(group['IpPermissions']):
+        permission['id'] = group['GroupId']
+        permission['type'] = "ingress"
+        permission['name'] = group['GroupName']
+        output += render(permission, 'aws_security_group_rule.tf')
+
+    for permission in group['IpPermissionsEgress']:
+        permission['id'] = group['GroupId']
+        permission['type'] = "egress"
+        permission['name'] = group['GroupName']
+        output += render(permission, 'aws_security_group_rule.tf')
+
+    formatted = fmt(output)
+
+    print(formatted)
 
 
-def output(resource, template_path):
+def count():
+    global counter
+    counter = counter + 1
+    return counter
+
+
+def reset_count():
+    global counter
+    counter = 0
+
+
+def render(resource, template_path):
     template_loader = jinja2.FileSystemLoader(searchpath="./templates")
     template_env = jinja2.Environment(loader=template_loader)
+    template_env.globals['count'] = count
     template = template_env.get_template(template_path)
-    output = template.render(resource=resource)
-    print(output)
+    rendered = template.render(resource=resource)
+    return rendered
 
 
-cli.add_command(get_security_group)
+def fmt(content):
+    formatted = subprocess.run(["terraform", "fmt", "-"], stdout=subprocess.PIPE, input=content, encoding='ascii')
+    return formatted.stdout
+
+
+cli.add_command(sg)
 
 if __name__ == "__main__":
     cli()
